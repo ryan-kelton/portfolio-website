db962ab1b6900fff04b218d73b9413b47ff2792f
Ryan Kelton <ryankelton@gmail.com>
2022-11-01 16:10:06 -0400

    index styles

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 "use strict";
 
 // Variables
-let mobileNav = document.getElementById("mobile-nav");
+let mobileNav = document.getElementById("main-nav");
 let hamburgerIcon = document.getElementById("hamburger-icon");
 let exitIcon = document.getElementById("exit-icon");
+let content = document.querySelector("main");
 
 // When hamburger menu is clicked turn on mobile navigation styles
 function hamburger(){
     mobileNav.classList.add("mobile-nav-styles");
-    mobileNav.classList.remove("hide");
+    mobileNav.classList.remove("hide-nav");
     hamburgerIcon.classList.add("hide");
     exitIcon.classList.remove("hide");
+    content.classList.add("dark-overlay");
 }
 hamburgerIcon.addEventListener("click", hamburger);
 
 // When the exit icon is clicked turn off mobile navigation styles
 function exit(){
     mobileNav.classList.remove("mobile-nav-styles");
-    mobileNav.classList.add("hide");
+    mobileNav.classList.add("hide-nav");
     exitIcon.classList.add("hide");
     hamburgerIcon.classList.remove("hide");
+    content.classList.remove("dark-overlay");
 }
 exitIcon.addEventListener("click", exit);
